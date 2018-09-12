46a409c5c22c84db303dfe5164cbc5d9ace1d746
赵泊洋 <zhaoby@cmrh.com>
2018-09-12 14:25:19 +0800

    创建脚手架和基本目录结构

---
+++ financial-regulation/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: url('assets/img/bg.png');
+  background-size: cover;
+  height: 100vh;
+}
+</style>
